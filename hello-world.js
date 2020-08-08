d853a089602837f4e904ba746f3feb4a3966e1a9
skadthan <skadthan@gmail.com>
2020-08-07 23:53:23 -0400

    adding new web scraping feature

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,29 @@
 console.log("Hello Ashu!\n");
+
+const axios = require('axios');
+const Nightmare = require('nightmare');
+const cheerio = require('cheerio');
+
+const url = 'https://news.ycombinator.com';
+ axios.get(url)
+    .then(response => {
+        let ycombinator_html_data = response.data;
+       // console.log(ycombinator_html_data);
+        console.log(getData(ycombinator_html_data));
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+
+    let getData = ycombinator_html_data => {
+        data = [];
+        const $ = cheerio.load(ycombinator_html_data);
+        $('table.itemlist tr td:nth-child(3)').each((i,elem)=> {
+            data.push({
+                title: $(elem).text(),
+                link: $(elem).find('a.storylink').attr('href')
+            });
+        });
+        console.log(data);
+    }
